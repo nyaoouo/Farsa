43a19c051ba98935708f88e5339768e10010e2db
ny <1>
2022-03-24 21:40:05 +0800

    setup details add

--- setup.py
+++ setup.py
@@ -4,5 +4,9 @@ setup(
     name='Farsa',
     version='0.1',
     packages=find_packages(),
-    install_requires=['pefile~=2021.9.3',]
+    install_requires=['pefile~=2021.9.3', ],
+    url='https://github.com/nyaoouo/Farsa',
+    license='GPLv3',
+    author='Nyaoouo',
+    author_email='-',
 )
